7c36b87efee832569dec810237f96f4abb846dd2
Hayato Kamiyama <kami8ma8810@gmail.com>
2023-01-05 21:47:26 +0900

    chore: pushエラー回避

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,13 +1,14 @@
 import { GetServerSideProps } from 'next';
 import { getAllPosts } from '~/lib/notion';
-import { generateRss } from '~/lib/rss';
+
+// import { generateRss } from '~/lib/rss';
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   res.setHeader('Content-Type', 'text/xml');
   const posts = await getAllPosts({ includedPages: false });
   const recentPublishPosts = posts.slice(0, 10);
-  const xmlFeed = generateRss(recentPublishPosts);
-  res.write(xmlFeed);
+  // const xmlFeed = generateRss(recentPublishPosts);
+  // res.write(xmlFeed);
   res.end();
   return {
     props: {},
